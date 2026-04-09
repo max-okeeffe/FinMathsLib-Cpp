269479723c52e870698f927c60003f747c6eefea
max-okeeffe <maximilian.okeeffe@kcl.ac.uk>
2026-04-09 15:34:50 +0100

    Initial commit: set up library skeleton with CMake and GoogleTest

---
+++ examples/pricing_example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Financial Maths Library Example" << std::endl;
+    return 0;
+}
---
+++ src/dummy.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 0;
+    int y = x;
+    cout << "x equals " << x << " and y equals " << y << endl;
+}
