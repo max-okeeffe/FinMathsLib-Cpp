869260c44f5489330ecea546ea866ffb9bc598ca
max-okeeffe <maximilian.okeeffe@kcl.ac.uk>
2026-04-11 00:06:28 +0100

    feat: add numerical differentiation with symmetric difference and adaptive step size

--- include/FinMaths/maths/calculus.hpp
+++ include/FinMaths/maths/calculus.hpp
@@ -1,10 +1,18 @@
 #ifndef FINMATHS_CALCULUS_HPP
 #define FINMATHS_CALCULUS_HPP
 
+#include <cmath>
+#include <concepts>
+#include <limits>
+
 namespace FinMaths::Maths {
 
-double differentiateNumerically();
+template<std::invocable<double> F>
+double differentiateNumerically(F f, double x) {
+    double h = std::sqrt(std::numeric_limits<double>::epsilon()) * (std::abs(x) + 1.0);
+    return (f(x + h) - f(x - h)) / (2.0 * h);
+}
 
-} // namespce FinMaths::Maths
+}  // namespace FinMaths::Maths
 
 #endif
--- src/maths/calculus.cpp
+++ src/maths/calculus.cpp
@@ -1,10 +1,7 @@
 #include "FinMaths/maths/calculus.hpp"
-#include <stdexcept>
 
 namespace FinMaths::Maths {
 
-double differentiateNumerically() {
-    throw std::logic_error("");
-}
 
-} // namespace FinMaths::Maths
+
+}  // namespace FinMaths::Maths
