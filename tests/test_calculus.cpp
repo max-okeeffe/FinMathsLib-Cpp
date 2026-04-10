8341f1eefc3ad3510642a12d7a8d3e6edf1b788e
max-okeeffe <maximilian.okeeffe@kcl.ac.uk>
2026-04-11 00:06:28 +0100

    feat: add numerical differentiation with symmetric difference and adaptive step size

--- tests/test_calculus.cpp
+++ tests/test_calculus.cpp
@@ -1,9 +1,27 @@
 #include <gtest/gtest.h>
 
+#include <numbers>
+
 #include "FinMaths/maths/calculus.hpp"
 
 using namespace FinMaths::Maths;
 
-TEST(DifferentiateNumerically, Pass) {
-    EXPECT_NO_THROW(differentiateNumerically());
+TEST(DifferentiateNumerically, Quadratic) {
+    auto f = [](double x) { return x * x; };
+    EXPECT_NEAR(differentiateNumerically(f, 3.0), 6.0, 1e-8);
+    EXPECT_NEAR(differentiateNumerically(f, 0.0), 0.0, 1e-8);
+    EXPECT_NEAR(differentiateNumerically(f, -2.0), -4.0, 1e-8);
+}
+
+TEST(DifferentiateNumerically, Sine) {
+    auto f = [](double x) { return std::sin(x); };
+    EXPECT_NEAR(differentiateNumerically(f, 0.0), 1.0, 1e-8);
+    EXPECT_NEAR(differentiateNumerically(f, std::numbers::pi / 2.0), 0.0, 1e-8);
+}
+
+TEST(DifferentiateNumerically, Exponential) {
+    auto f = [](double x) { return std::exp(x); };
+    EXPECT_NEAR(differentiateNumerically(f, 1.0), std::exp(1.0), 1e-8);
+    EXPECT_NEAR(differentiateNumerically(f, 0.0), 1.0, 1e-8);
+    EXPECT_NEAR(differentiateNumerically(f, -1.0), std::exp(-1.0), 1e-8);
 }
