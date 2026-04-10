240695cd8aadd1d845357b31462ab6b01b75bb54
max-okeeffe <maximilian.okeeffe@kcl.ac.uk>
2026-04-10 23:54:36 +0100

    build: add skeleton for calculus module

---
+++ include/FinMaths/maths/calculus.hpp
@@ -0,0 +1,10 @@
+#ifndef FINMATHS_CALCULUS_HPP
+#define FINMATHS_CALCULUS_HPP
+
+namespace FinMaths::Maths {
+
+double differentiateNumerically();
+
+} // namespce FinMaths::Maths
+
+#endif
---
+++ src/maths/calculus.cpp
@@ -0,0 +1,10 @@
+#include "FinMaths/maths/calculus.hpp"
+#include <stdexcept>
+
+namespace FinMaths::Maths {
+
+double differentiateNumerically() {
+    throw std::logic_error("");
+}
+
+} // namespace FinMaths::Maths
