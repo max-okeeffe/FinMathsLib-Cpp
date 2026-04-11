973616fd8f844e78e660f0d4fc53f0eac63aa683
max-okeeffe <maximilian.okeeffe@kcl.ac.uk>
2026-04-11 19:34:19 +0100

    build add matrix module scaffolding

---
+++ include/FinMaths/maths/matrix.hpp
@@ -0,0 +1,10 @@
+#ifndef FINMATHS_MATRIX_HPP
+#define FINMATHS_MATRIX_HPP
+
+namespace FinMaths::Maths {
+
+
+
+} // namespace FinMaths::Maths
+
+#endif
---
+++ tests/test_matrix.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "FinMaths/maths/matrix.hpp"
+
+using namespace FinMaths::Maths;
+
+
